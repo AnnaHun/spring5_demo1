076a9cd2666526dc423484a771e7bf3310777459
zhuzhaoming <18327510516@163.com>
2021-08-30 21:13:42 +0800

    spring_restudy_day03

---
+++ spring5_tx_demo1/src/service/UserDao.java
@@ -0,0 +1,16 @@
+package service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Intellij IDEA.
+ *
+ * @author ZhuZhaoMing
+ * @Description
+ * @date 2021/8/30-9:07 下午
+ * @motto: Never say die Never give up
+ */
+@Service
+public class UserDao {
+
+}
